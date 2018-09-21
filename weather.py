152c00bc1050a0d26b6d7c24b94267d9129f36f0
AaronPengwp <AaronPengwp@foxmail.com>
2018-09-21 14:56:12 +0800

    City weather enquiry

---
+++ weather.py
@@ -0,0 +1,23 @@
+# -*- coding: gbk -*-
+
+import urllib.request
+import json
+from city import city
+
+cityname = input('你想查哪种个城市的天气？\n')
+citycode = city.get(cityname)
+
+if citycode:
+    try:
+        url = ('http://www.weather.com.cn/data/cityinfo/%s.html' % citycode)
+        #print(url)
+        content = urllib.request.urlopen(url).read()
+        data = json.loads(content)
+        result = data['weatherinfo']
+        str_temp = ('%s\n%s ~ %s')%(
+            result['weather'],result['temp1'],result['temp2'])
+        print(str_temp)
+    except:
+        print('查询失败')
+else:
+    print('没有长到该城市')
